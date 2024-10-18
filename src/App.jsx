c50b645a01e828f3c29dc0149f4657f567a7e647
kenshiro8 <kenshiro.160818@gmail.com>
2024-10-18 17:08:34 +0900

    implement random function

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import randomValueFromArray from "./components/Random"
 
 export default function App() {
   const showStory = false;
-  const xItem = "";
-  const yItem = "";
-  const zItem = "";
+  const xItem = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+  const yItem = ["the soup kitchen", "Disneyland", "the White House"];
+  const zItem = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
   const ukus = "us";
+  let xItem_num = <randomValueFromArray/ >;
+  let yItem_num = <randomValueFromArray/ >;
+  let zItem_num = <randomValueFromArray/ >;
+
   return (
     <>
       <div>
@@ -23,9 +27,9 @@ export default function App() {
       </div>
       {showStory && (
         <p>
-          It was 94 fahrenheit outside, so {xItem} went for a walk. When they
-          got to {yItem}, they stared in horror for a few moments, then {zItem}.
-          Bob saw the whole thing, but was not surprised — {xItem} weighs 300
+          It was 94 fahrenheit outside, so {xItem[xItem_num]} went for a walk. When they
+          got to {yItem[yItem_num]}, they stared in horror for a few moments, then {zItem[zItem_num]}.
+          Bob saw the whole thing, but was not surprised — {xItem[xItem_num]} weighs 300
           pounds, and it was a hot day.
         </p>
       )}
