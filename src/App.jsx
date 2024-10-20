8cafeb8661251350cb5baef924e340e882b00585
kenshiro8 <kenshiro.160818@gmail.com>
2024-10-20 12:08:07 +0900

    ukusの数値と単位の分岐の実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react"
 import randomValueFromArray from "./components/Random"
-import branchWeightUnit from "./components/Branch"
-import branchTemperatureUnit from "./components/Branch"
+import {branchWeightUnit, branchTemperatureUnit} from "./components/Branch"
 
 export default function App() {
   const xItem = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
   const yItem = ["the soup kitchen", "Disneyland", "the White House"];
   const zItem = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-  const [showStory, setshowStory] = useState(false);
+  const [showStory, setShowStory] = useState(false);
   const [customName, setCustomName] = useState("");
   const [tempCustomName, setTempCustomName] = useState("");
   const [ukus, setUkus] = useState("us");
+  const [tempUkus, setTempUkus] = useState("us");
 
   const [xItem_num, setxItem_num] = useState(0);
   const [yItem_num, setyItem_num] = useState(0);
@@ -21,12 +21,17 @@ export default function App() {
     setxItem_num(randomValueFromArray(xItem));
     setyItem_num(randomValueFromArray(yItem));
     setzItem_num(randomValueFromArray(zItem));
-    setshowStory(true);
+    setShowStory(true);
     setCustomName(tempCustomName);
+    setUkus(tempUkus);
   }
 
   const handleCustomName = (e) => {
     setTempCustomName(e.target.value);
+  }
+
+  const handleUkusChange = (e) => {
+    setTempUkus(e.target.value);
   }
 
   return (
@@ -43,13 +48,13 @@ export default function App() {
         <label htmlFor="us">US</label>
         <input type="radio" 
         value="us" 
-        checked={ukus === "us"}  
-        onChange={() => setUkus("us")}/>
+        checked={tempUkus === "us"}  
+        onChange={handleUkusChange}/>
         <label htmlFor="uk">UK</label>
         <input type="radio" 
-        value="uk" 
-        checked={ukus === "uk"} 
-        onChange={() => setUkus("uk")}/>
+        value="uk"  
+        checked={tempUkus === "uk"} 
+        onChange={handleUkusChange}/>
       </div>
 
       <div>
@@ -58,10 +63,10 @@ export default function App() {
 
       {showStory && (
         <p>
-          It was 94 { branchTemperatureUnit(ukus) } outside, so {xItem[xItem_num]} went for a walk. When they
-          got to {yItem[yItem_num]}, they stared in horror for a few moments, then {zItem[zItem_num]}.
-          {" "}{customName ? customName : "Bob"} saw the whole thing, but was not surprised — {xItem[xItem_num]} weighs 300
-          {branchWeightUnit(ukus) }, and it was a hot day.
+          It was { branchTemperatureUnit(ukus) } outside, so {xItem[xItem_num]} went for a walk. When they
+          got to {yItem[yItem_num]}, they stared in horror for a few moments,
+          then {zItem[zItem_num]}. {customName ? customName : "Bob"} saw the whole thing,
+          but was not surprised — {xItem[xItem_num]} weighs {branchWeightUnit(ukus) }, and it was a hot day.
         </p>
       )}
     </>
